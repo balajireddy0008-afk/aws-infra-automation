a8a78c085edc93ceffa3643afc673d885b6256eb
Balaji <balajireddy0008@gmail.com>
2026-06-02 15:24:23 +0530

    Move backend configuration into terraform directory

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "balaji-terraform-s3"
-    key            = "aws-infra-automation/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
